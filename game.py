6d357f830b42d7e8c7fbdfa5a334d7e6fccde262
Billy den Ouden <152157601+amstelvenia@users.noreply.github.com>
2025-05-28 13:15:46 +0200

    Opdracht 1 - het scherm

---
+++ game.py
@@ -0,0 +1,10 @@
+import turtle
+
+# Venster instellen
+wn = turtle.Screen()
+wn.title("Pong voor Billy")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+input("Press any key to continue...") # tijdelijke toevoeging t.b.v. testen
