de63af3ea7dd5e605d637f0af469520ad85a8587
Billy den Ouden <152157601+amstelvenia@users.noreply.github.com>
2025-05-28 13:22:05 +0200

    Opdracht 2 - Paddle

--- game.py
+++ game.py
@@ -7,4 +7,14 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle
+paddle = turtle.Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=6, stretch_len=1)
+paddle.penup()
+paddle.goto(-350,0)
+
+wn.update()
+
 input("Press any key to continue...") # tijdelijke toevoeging t.b.v. testen
